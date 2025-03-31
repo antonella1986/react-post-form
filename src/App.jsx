27926666d03c58712239437398a05afbc1743931
antonella1986 <antonella.leo.mail@gmail.com>
2025-03-31 15:14:36 +0200

    add name, onChange and checked in the inputs

--- src/App.jsx
+++ src/App.jsx
@@ -29,21 +29,48 @@ function App() {
       <form>
           <div class="mb-3">
               <label htmlFor="author" className="form-label">Autore</label>
-              <input type="text" className="form-control" id="author" placeholder="Inserisci il nome dell'autore">
+              <input
+              type="text"
+              className="form-control"
+              id="author"
+              placeholder="Inserisci il nome dell'autore"
+              name="author"
+              onChange={handleChange}
+              />
           </div>
           
           <div class="mb-3">
               <label htmlFor="title" className="form-label">Titolo</label>
-              <input type="text" className="form-control" id="title" placeholder="Inserisci il titolo">
+              <input
+              type="text"
+              className="form-control"
+              id="title"
+              placeholder="Inserisci il titolo"
+              name="title"
+              onChange={handleChange}
+              />
           </div>
           
           <div class="mb-3">
               <label htmlFor="body" className="form-label">Corpo del Post</label>
-              <textarea className="form-control" id="body" rows="5" placeholder="Scrivi il contenuto del post"></textarea>
+              <textarea
+              className="form-control"
+              id="body"
+              rows="5"
+              placeholder="Scrivi il contenuto del post"
+              name="body"
+              onChange={handleChange}></textarea>
           </div>
           
           <div class="form-check form-switch mb-3">
-              <input className="form-check-input" type="checkbox" id="public">
+              <input
+              className="form-check-input"
+              type="checkbox"
+              id="public"
+              checked={formData.public}
+              name="public"
+              onChange={handleChange}
+              />
               <label className="form-check-label" htmlFor="public">Pubblico</label>
           </div>
           
